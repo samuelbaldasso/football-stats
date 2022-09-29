6479a39df8d88a74eae5e1b45ab283f5424364a3
Samuel Baldasso <sbaldass@MacBook-Pro-de-IT.local>
2022-09-29 14:18:51 -0300

    feat: input de dados rodando para times e jogadores

---
+++ src/app/controller/filterTeams.pipe.ts
@@ -0,0 +1,14 @@
+import { Jogador } from './../model/jogador';
+import { Pipe, PipeTransform } from '@angular/core';
+import { Times } from './../model/times';
+
+@Pipe({ name: 'filterTeams' })
+export class FilterTeamsPipe implements PipeTransform {
+    transform(teams: Times[], searchText: string) {
+        if(teams.length === 0 || searchText === ''){
+          return teams;
+        }else{
+          return teams.filter((team) => team.name.toLowerCase() === searchText.toLowerCase());
+        }
+      }
+  }
